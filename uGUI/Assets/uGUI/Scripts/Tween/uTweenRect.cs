2765570c9873ad87b66edf56e0cd022a1145634a
Tom <997063802@qq.com>
2015-12-19 22:35:40 +0800

    add TweenRect&TweenImage Update demo scene

---
+++ uGUI/Assets/uGUI/Scripts/Tween/uTweenRect.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace uTools {
+	[AddComponentMenu("uTools/Tween/Tween Width(uTools)")]	
+	
+	public class uTweenRect : uTweener {
+
+		public Vector2 from;
+		public Vector2 to;
+
+		Vector2 mValue;
+		public Vector2 value {
+			get { return mValue;}
+			set { 
+				mValue = value;
+			}
+		}
+
+		private RectTransform mRectTransform;
+		public RectTransform cacheRectTransform {
+			get {
+				if (mRectTransform == null) {
+					mRectTransform = GetComponent<RectTransform>();
+				}
+				return mRectTransform;
+			}
+		}
+
+		protected override void OnUpdate (float factor, bool isFinished) {
+			value = from + factor * (to - from);
+			cacheRectTransform.sizeDelta = value;
+		}
+
+		public static uTweenRect Begin(RectTransform go, float duration, float delay, Vector2 from, Vector2 to) {
+			uTweenRect comp = uTweener.Begin<uTweenRect>(go.gameObject, duration);
+			comp.from = from;
+			comp.to = to;
+			comp.delay = delay;
+			
+			if (duration <=0) {
+				comp.Sample(1, true);
+				comp.enabled = false;
+			}
+			return comp;
+		}
+	}
+}
