3114430d7d75b7f95f28eb7b9f26370489cbc978
tomtc123 <997063802@qq.com>
2016-10-17 14:13:57 +0800

    optimize

--- uGUI/Assets/uGUI/Scripts/Tween/uTweenShake.cs
+++ uGUI/Assets/uGUI/Scripts/Tween/uTweenShake.cs
@@ -47,6 +47,28 @@ namespace uTools
             }
         }
 
+        Transform mTransform = null;
+        RectTransform mRectTransform = null;
+        bool mIs3D = true;
+        private bool is3D
+        {
+            get
+            {
+                if (mTransform == null)
+                {
+                    //init once
+                    mTransform = transform;
+                    mRectTransform = target as RectTransform;
+                    mIs3D = (mRectTransform != null) ? false : true;
+                }
+                return mIs3D;
+            }
+            set
+            {
+                mIs3D = value;
+            }
+        }
+
         private void CacheTargetInfo()
         {
             localPosition = target.localPosition;
@@ -59,12 +81,12 @@ namespace uTools
         protected override void Start()
         {
             base.Start();
-            CacheTargetInfo();
         }
 
 
         protected override void OnUpdate(float factor, bool isFinished)
         {
+            factor = 1 - factor;
             float x = limit.x * factor;
             float y = limit.y * factor;
             float z = limit.z * factor;
@@ -74,17 +96,26 @@ namespace uTools
             value = mValue;
         }
 
+        private Vector3 tempVector3 = Vector3.one;
         void Shake()
         {
             if (shakeType == eShake.Position)
             {
                 if (space == Space.Self)
                 {
-                    target.localPosition = value + localPosition;
+                    tempVector3 = value + localPosition;
+                    if (is3D)
+                        target.localPosition = tempVector3;
+                    else
+                        mRectTransform.anchoredPosition = tempVector3;
                 }
                 else
                 {
-                    target.position = value + position;
+                    tempVector3 = value + position;
+                    if (is3D)
+                        target.position = tempVector3;
+                    else
+                        mRectTransform.anchoredPosition3D = tempVector3;
                 }
             }
             else if (shakeType == eShake.Scale)
